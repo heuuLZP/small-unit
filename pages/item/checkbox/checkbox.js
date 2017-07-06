4cd6a9f4c612a37282c9d61644f76d0bffdea281
wutong <heuulzp@163.com>
2017-07-06 18:18:43 +0800

    添加checkbox组件

--- pages/item/checkbox/checkbox.js
+++ pages/item/checkbox/checkbox.js
@@ -1,11 +1,19 @@
-// scroll-view.js
+// checkbox.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    items: [
+      { name: 'USA', value: '美国' },
+      { name: 'CHN', value: '中国'},
+      { name: 'BRA', value: '巴西' },
+      { name: 'JPN', value: '日本' },
+      { name: 'ENG', value: '英国' },
+      { name: 'TUR', value: '法国' },
+    ],
+    print: '当前选择的是'
   },
 
   /**
@@ -19,69 +27,61 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+  
+  },
 
-  },
-   /**
-   * scroll
-   */
-  scroll: function (e) {
-    // console.log(e);
-    console.log('滚啊滚^_^');
-  },
   /**
-   * lower
+   * 选择checkbox
    */
-  lower: function (e) {
-    // console.log(e);
-    console.log('滚到底部了...');
-  },
-  /**
-   * upper
-   */
-  upper: function (e) {
-    // console.log(e);
-    console.log('滚到顶部了...');
+  bindchange: function (e) {
+    let str = '当前选择的是';
+    for (let item of e.detail.value) {
+      str += item + ','
+    }
+    this.setData({
+      print: str,
+    })
   }
 })
